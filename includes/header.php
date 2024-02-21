d93b66e26599557159f9e8619cc33094c319dbb1
Olivier <ol.maignan@gmail.com>
2024-02-21 13:46:18 +0100

    creation du fichier traitementPhp et section admin en cours

--- includes/header.php
+++ includes/header.php
@@ -8,6 +8,7 @@
   <link rel="stylesheet" href="../assets/style.css">
   <link rel="stylesheet" href="../assets/header.css">
   <link rel="stylesheet" href="../assets/formulaire.css">
+  <link rel="stylesheet" href="../assets/sectionAdmin.css">
   <script src="../assets/script.js" defer></script>
 </head>
 
@@ -15,6 +16,6 @@
 
   <div class="header">
     <a class = "btn1" href="connexion.php">Connexion</a>
-    <a class = "btn2" href="sectionAdmin.php">Admin</a>
+    <a class = "btn2" href="./includes/sectionAdmin.php">Admin</a>
   </div>
 
