71480febf842d7f457a2106a07613990b9e1850e
Mélissa <melissa.soares1909@gmail.com>
2024-02-21 10:08:15 +0100

    création nvx fichiers css

--- includes/header.php
+++ includes/header.php
@@ -1,12 +1,20 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Formulaire de réservation Music Vercos Festival</title>
   <link rel="stylesheet" href="../assets/style.css">
+  <link rel="stylesheet" href="../assets/header.css">
+  <link rel="stylesheet" href="../assets/formulaire.css">
   <script src="../assets/script.js" defer></script>
 </head>
+
 <body>
 
-//ici il faut ajouter notre header
+  <div class="header">
+    <a class = "btn1" href="connexion.php">Connexion</a>
+    <a class = "btn2" href="sectionAdmin.php">Admin</a>
+  </div>
+
