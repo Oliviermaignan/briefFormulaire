7338fbb0286e2e732eb2840dd76651971877c05e
Olivier <ol.maignan@gmail.com>
2024-02-22 09:33:02 +0100

    mise du du fichier indexPhp

--- index.php
+++ index.php
@@ -2,8 +2,6 @@
 
 include './includes/header.php';
 include './includes/formulaire.php';
-//ici on doit faire du code php
-include './includes/sectionAdmin.php';
 include './includes/footer.php';
 
 ?>
