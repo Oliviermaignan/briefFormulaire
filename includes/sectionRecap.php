a3426ec4e01c46081cc6b12d54e9db8b7be91be5
Mélissa <melissa.soares1909@gmail.com>
2024-02-22 16:32:30 +0100

    ajout sectionRecap.php

---
+++ includes/sectionRecap.php
@@ -0,0 +1,17 @@
+<?php 
+
+include '../includes/header.php';
+include '../src/traitement.php';
+include '../src/classes/User.php';
+include '../src/classes/Reservation.php';
+
+?>
+
+<div class = "recap">
+    <p> Merci pour votre réservation. À très bientot ! </p>
+    <?php 
+
+    // $user = $_id, $_nom, $_prenom, $_mail, $_phone, $_adressePost ;
+
+    ?>
+</div>
