4096f8e4d86944e54c7040b76c827eb5049a573c
Olivier <ol.maignan@gmail.com>
2024-02-22 11:07:54 +0100

    creation databaseReservation et reservationCSV et test ecriture en base sur utilisateurCSV

--- index.php
+++ index.php
@@ -3,5 +3,14 @@
 include './includes/header.php';
 include './includes/formulaire.php';
 include './includes/footer.php';
+include './src/classes/Database.php';
+include './src/classes/User.php';
+
+$nouveauUser = new User ('olivier', 'aaa', 'aaa@aaa.ff', 'aaa', 1, 'admin');
+var_dump($nouveauUser);
+$newDatabase = new Database();
+var_dump ($newDatabase);
+$newDatabase->saveUtilisateur($nouveauUser);
+
 
 ?>
